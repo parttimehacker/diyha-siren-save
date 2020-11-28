5d24483c05b80ab15fe3b64d85cd80ef8dda245f
Dave Wilson <parttimehacker@gmail.com>
2020-11-28 10:24:55 -0800

    change names and Adafruit stuff

--- import.script.sh
+++ import.script.sh
@@ -7,13 +7,7 @@ sudo pip3 install paho-mqtt
 sudo pip3 install psutil
 sudo pip3 install pylint
 sudo apt -y install screen
+sudo pip3 install pip3 install RPI.GPIO
+sudo pip3 install adafruit-blinka
+echo "Done -------------"
 
-echo "Install Adafruit stuff"
-git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
-cd Adafruit_Python_GPIO
-sudo python3 setup.py install
-cd ..
-git clone https://github.com/adafruit/Adafruit_Python_LED_Backpack.git 
-cd Adafruit_Python_LED_Backpack
-sudo python3 setup.py install
-cd ..
